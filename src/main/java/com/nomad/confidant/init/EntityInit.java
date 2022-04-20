b44467157322079ba8d9ec5ba54a2ee2170bb983
razgriz39 <razgrizosea@gmail.com>
2022-04-20 21:14:53 +0900

    Test branch

---
+++ src/main/java/com/nomad/confidant/init/EntityInit.java
@@ -0,0 +1,9 @@
+package com.nomad.confidant.init;
+
+import com.nomad.confidant.Confidant;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class EntityInit {
+}
